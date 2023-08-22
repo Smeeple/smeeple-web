b647be614adf94e234218a1f36f53e0b9ebf1607
Ryan Altvater <ryan@ryanaltvater.com>
2023-08-22 11:20:57 -0400

    Added logic to consolidate the 2 headers into 1

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,22 +1,47 @@
+<script setup>
+    useHead({
+        script: [
+            { src: 'assets/js/components/Header.js', tagPosition: 'bodyClose' }
+]
+    });
+</script>
+
 <template>
-    <div v-if="$root.hasClass">
-        <Header_Dark />
-    </div>
+    <header class="fixed top-0 z-50 w-full transition-all duration-300">
+        <Header_Banner />
 
-    <div v-else>
-        <Header_Light />
+        <div class="container py-2">
+            <div class="flex min-h-[52px] items-center justify-between">
+                <NuxtLink to="/">
+                    <Logo_Smeeple v-if="parentClass === 'header-light'" parentClass="logo-light" />
 
-        <p>Test</p>
-    </div>
+                    <Logo_Smeeple v-else-if="parentClass === 'header-dark'" :isHeaderBgBlue="isHeaderBgBlue" :isMobileNavOpen="isMobileNavOpen" parentClass="logo-dark" />
+                </NuxtLink>
+
+                <nav>
+                    <ul class="nav-list flex list-none items-center">
+                        <Nav_List v-if="parentClass === 'header-light'" parentClass="nav-header-light" />
+
+                        <Nav_List v-else-if="parentClass === 'header-dark'" parentClass="nav-header-dark" />
+                    </ul>
+                </nav>
+
+                <div :class="{ 'nav-toggle-open': isMobileNavOpen }" class="nav-toggle lg:hidden">
+                    <Icon_Menu v-if="parentClass === 'header-light'" @click="toggleMobileNav" parentClass="icon-light" />
+
+                    <Icon_Menu v-else-if="parentClass === 'header-dark'" :isHeaderBgBlue="isHeaderBgBlue" :isMobileNavOpen="isMobileNavOpen" @click="toggleMobileNav" parentClass="icon-dark" />
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <Nav_Mobile :class="{ '-right-[991px]': !isMobileNavOpen, 'right-0': isMobileNavOpen }" />
 </template>
 
 <script>
+    import Header from '~/plugins/components/Header';
+
     export default {
-        computed: {
-            isPageDark() {
-                // Access the $root data property to check if the "page-dark" class exists
-                return this.$root.hasClass;
-            },
-        },
+        mixins: [Header],
     };
 </script>
