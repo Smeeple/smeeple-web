9e11454bf84a4208939325333a28a7fa143e1f02
Ryan Altvater <ryan@ryanaltvater.com>
2023-08-22 11:22:28 -0400

    Added logic to use same list across various navs

--- src/components/navigation/Nav_List.vue
+++ src/components/navigation/Nav_List.vue
@@ -1,25 +1,41 @@
 <template>
-    <li v-if="$route.path === '/for-experts'">
-        <NuxtLink to="/">For consumers</NuxtLink>
+    <li v-if="$route.path !== '/for-experts'" :class="{ 'md-max:hidden': parentClass.startsWith('nav-header') }">
+        <NuxtLink :class="{ 'text-white': parentClass === 'nav-header-light' || isHeaderBgBlue, 'text-blue': parentClass === 'nav-header-dark' }" class="transition-all duration-150 hover:text-orange-light" to="/for-experts">For experts</NuxtLink>
     </li>
 
-    <li v-if="$route.path !== '/for-experts'">
-        <NuxtLink to="/for-experts">For experts</NuxtLink>
+    <li v-if="$route.path === '/for-experts'" :class="{ 'md-max:hidden': parentClass.startsWith('nav-header') }">
+        <NuxtLink :class="{ 'text-blue': !isHeaderBgBlue, 'text-white': isHeaderBgBlue }" class="transition-all duration-150 hover:text-orange-light" to="/">For consumers</NuxtLink>
     </li>
 
-    <li v-if="$route.path === '/for-experts'" class="mt-1">
-        <NuxtLink to="https://app.smeeple.com/sign-in">Expert login</NuxtLink>
+    <li :class="{ 'ml-4 md-max:hidden': parentClass.startsWith('nav-header'), 'mt-1': parentClass.startsWith('nav-footer') || parentClass === 'nav-mobile' }">
+        <NuxtLink :class="{ 'text-white': parentClass === 'nav-header-light' || isHeaderBgBlue, 'text-blue': parentClass === 'nav-header-dark' }" class="transition-all duration-150 hover:text-orange-light" to="/about-smeeple">About Smeeple</NuxtLink>
     </li>
 
-    <li class="mt-1">
-        <NuxtLink to="/about-us">About us</NuxtLink>
+    <li :class="{ 'ml-4 md-max:hidden': parentClass.startsWith('nav-header'), 'mt-1': parentClass.startsWith('nav-footer') || parentClass === 'nav-mobile' }">
+        <NuxtLink :class="{ 'text-white': parentClass === 'nav-header-light' || isHeaderBgBlue, 'text-blue': parentClass === 'nav-header-dark' }" class="transition-all duration-150 hover:text-orange-light" to="/faqs">FAQs</NuxtLink>
     </li>
 
-    <li class="mt-1">
-        <NuxtLink to="/support">Support</NuxtLink>
+    <li :class="{ 'ml-4 md-max:hidden': parentClass.startsWith('nav-header'), 'mt-1': parentClass.startsWith('nav-footer') || parentClass === 'nav-mobile' }">
+        <NuxtLink :class="{ 'text-white': parentClass === 'nav-header-light' || isHeaderBgBlue, 'text-blue': parentClass === 'nav-header-dark' }" class="transition-all duration-150 hover:text-orange-light" to="/support">Support</NuxtLink>
     </li>
 
-    <li class="mt-1">
-        <NuxtLink to="/contact-us">Contact us</NuxtLink>
+    <li :class="{ 'ml-4 md-max:hidden': parentClass.startsWith('nav-header'), 'mt-1': parentClass.startsWith('nav-footer') || parentClass === 'nav-mobile' }">
+        <NuxtLink :class="{ 'text-white': parentClass === 'nav-header-light' || isHeaderBgBlue, 'text-blue': parentClass === 'nav-header-dark' }" class="transition-all duration-150 hover:text-orange-light" to="/contact-us">Contact us</NuxtLink>
+    </li>
+
+    <li v-if="parentClass.startsWith('nav-header') && $route.path === '/for-experts'" :class="{ 'ml-4 md-max:hidden': parentClass.startsWith('nav-header') }">
+        <NuxtLink class="cta-primary flex items-center" to="https://app.smeeple.com/sign-in">
+            <Icon_Locked class="mr-1 fill-white" />
+
+            Expert login
+        </NuxtLink>
     </li>
 </template>
+
+<script>
+    import Header from '~/plugins/components/Header';
+
+    export default {
+        mixins: [Header],
+    };
+</script>
