9787d33410983cffd5414035e2f6e542901ad448
Ryan Altvater <ryan@ryanaltvater.com>
2023-08-22 11:21:21 -0400

    Added 3 new icons

--- src/components/icons/Icon_Menu.vue
+++ src/components/icons/Icon_Menu.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="nav-toggle top-{3px} relative rotate-180 hover:cursor-pointer">
-        <span class="nav-toggle-item block bg-white transition-all duration-300"></span>
-        <span class="nav-toggle-item block bg-white transition-all duration-300"></span>
-        <span class="nav-toggle-item block bg-white transition-all duration-300"></span>
+    <div class="relative top-[3px] rotate-180 hover:cursor-pointer">
+        <span
+            v-for="index in 3"
+            :key="index"
+            :class="{
+                'bg-blue': parentClass === 'icon-dark' && !isHeaderBgBlue && !isMobileNavOpen,
+                'bg-white': parentClass === 'icon-light' || isHeaderBgBlue || isMobileNavOpen,
+            }"
+            class="nav-toggle-bar block transition-all duration-300"></span>
     </div>
 </template>
+
+<script>
+    export default {
+        props: {
+            isHeaderBgBlue: Boolean,
+            isMobileNavOpen: Boolean,
+            parentClass: String,
+        },
+    };
+</script>
