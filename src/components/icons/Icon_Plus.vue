4a2766734c56fa83ef1a318890a91387924c64f9
Ryan Altvater <ryan@ryanaltvater.com>
2023-08-23 11:14:01 -0400

    Added 2 icons, plus and minus

---
+++ src/components/icons/Icon_Plus.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 16 16">
+        <path d="M15,7H9V1c0-0.6-0.4-1-1-1S7,0.4,7,1v6H1C0.4,7,0,7.4,0,8s0.4,1,1,1h6v6c0,0.6,0.4,1,1,1s1-0.4,1-1V9h6 c0.6,0,1-0.4,1-1S15.6,7,15,7z"></path>
+    </svg>
+</template>
