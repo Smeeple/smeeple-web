9e775358397a75ac4ea53b1f5115d5ee6368e187
Ryan Altvater <ryan@ryanaltvater.com>
2023-10-03 21:58:23 -0400

    Resolved a type color issue

--- src/components/layout/Newsletter_Signup.vue
+++ src/components/layout/Newsletter_Signup.vue
@@ -2,7 +2,7 @@
     <section class="border-t border-gray-dark bg-blue-light bg-[url('/public/assets/img/bg-dot.png')] bg-bottom sm-max:pb-5 sm-max:pt-4">
         <div class="container">
             <div class="sm:grid sm:grid-cols-12 sm:gap-3">
-                <div class="text-center text-white sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
+                <div class="text-center sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
                     <h2 class="leading-tight text-white">Subscribe to our newsletter</h2>
 
                     <div class="mb-3 flex items-center justify-center">
@@ -13,7 +13,7 @@
                         <NuxtLink class="cta-tertiary ml-2" to="#">Sign up</NuxtLink>
                     </div>
 
-                    <p>Get the latest Smeeple news and updates delivered<br />directly to your inbox every month.</p>
+                    <p class="text-white">Get the latest Smeeple news and updates delivered<br />directly to your inbox every month.</p>
                 </div>
             </div>
         </div>
