a222b770dc1b73bf4defa55fae80028f44554b67
Ryan Altvater <ryan@ryanaltvater.com>
2023-10-03 21:06:45 -0400

    Added new layouts

---
+++ src/layouts/contact-us.vue
@@ -0,0 +1,43 @@
+<script setup>
+    useHead({
+        titleTemplate: (titleChunk) => {
+            return titleChunk ? `${titleChunk} | Smeeple` : 'Smeeple';
+        },
+    });
+</script>
+
+<template>
+    <Header parentClass="header-dark" />
+
+    <section class="bg-gray-light bg-[url('/assets/img/bg-shapes-gray.png')]">
+        <div class="container">
+            <div class="sm:grid sm:grid-cols-12 sm:gap-3">
+                <div class="sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
+                    <h1 class="mb-0">Contact us</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="sm:grid sm:grid-cols-12 sm:gap-3">
+                <div class="xs-max:mt-3 sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3 2xl:col-span-5 2xl:col-start-3">
+                    <h2>Send a message</h2>
+
+                    <p>Have a question, technical issue, or some feedback? Our team is here to assist you. Complete the form below, and we'll respond as soon as we can.</p>
+
+                    <div class="mt-3">
+                        <slot />
+                    </div>
+                </div>
+
+                <div class="xs-max:mt-3 sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3 2xl:col-span-3">
+                    <Contact_Sidebar />
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <Footer />
+</template>
--- src/layouts/interior.vue
+++ src/layouts/interior.vue
@@ -1,3 +1,11 @@
+<script setup>
+    useHead({
+        titleTemplate: (titleChunk) => {
+            return titleChunk ? `${titleChunk} | Smeeple` : 'Smeeple';
+        },
+    });
+</script>
+
 <template>
     <Header parentClass="header-dark" />
 
