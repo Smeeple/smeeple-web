655923214095bb1bc37467b990ed528b1dfc9fd6
Ryan Altvater <ryan@ryanaltvater.com>
2023-11-02 20:45:24 -0400

    Updated social icons

---
+++ src/components/icons/Icon_X.vue
@@ -0,0 +1,9 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <polygon class="st0" points="10.8,15.3 4.2,22.8 0.5,22.8 9.1,13" />
+
+        <polygon class="st0" points="12.8,8.1 18.9,1.2 22.6,1.2 14.5,10.4" />
+
+        <path class="st0" d="M24,22.8h-7.4L0,1.2h7.6L24,22.8z M17.6,20.6h2L6.5,3.2H4.3L17.6,20.6L17.6,20.6z" />
+    </svg>
+</template>
