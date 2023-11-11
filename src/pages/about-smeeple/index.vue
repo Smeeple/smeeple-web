07cdf9eb03e956684db992e776fe4c05a63e7e33
Ryan Altvater <ryan@ryanaltvater.com>
2023-11-11 11:25:58 -0500

    Update content and added video

--- src/pages/about-smeeple/index.vue
+++ src/pages/about-smeeple/index.vue
@@ -32,23 +32,29 @@
                         <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full" src="https://www.youtube-nocookie.com/embed/k6UZPlgQg84?mode=opaque&rel=0&autohide=1&showinfo=0&wmode=transparent" title="Signing up as an Expert is easy!" allowfullscreen></iframe>
                     </div>
 
-                    <h2>Two college friends embarked on a mission, to create the #1 community of&nbsp;experts</h2>
+                    <h2>Need solutions to unique problems?</h2>
 
-                    <figure class="sm-max:mx-auto sm-max:mb-3 sm-max:w-auto md:float-left md:mb-3 md:mr-3 md:mt-1 lg:w-[300px] xl:w-[360px]">
+                    <!-- <figure class="sm-max:mx-auto sm-max:mb-3 sm-max:w-auto md:float-left md:mb-3 md:mr-3 md:mt-1 lg:w-[300px] xl:w-[360px]">
                         <img class="mb-1 rounded-lg" src="/assets/img/photo-calvin-josh.jpg" alt="Photo of Calvin and Josh" />
 
                         <figcaption>
                             <cite>Calvin Holston (left) and Josh Mallalieu (right), graduating from Grove City College.</cite>
                         </figcaption>
-                    </figure>
+                    </figure> -->
 
-                    <p>Looking for solutions to unique problems? You're not alone. We've all been stumped by topics like a leaky faucet, the best way to improve our golf swing, or finding the right skincare routine for our skin type. While online resources can be helpful, they don't always provide the answers we need.</p>
+                    <p>You're not alone. We've all found ourselves puzzled by various topics at different points in our lives. It could be something as seemingly simple as a leaky faucet that just won't stop dripping, no matter what you try. Or perhaps it's the elusive perfect golf swing that you've been striving to master, spending countless hours at the driving range with little to no improvement. It could even be the quest for the ideal skincare routine that suits your unique skin type, a routine that balances hydration and oil control, and combats the signs of aging without causing breakouts.</p>
 
-                    <p>That's why we created Smeeple. Our community of experts covers a wide range of categories, from DIY projects to sports training to home decor and beyond. With just a few clicks, you can connect live with the expert who can help you solve your problem. No more endless searching or generic advice. Get personalized solutions that work for you with Smeeple.</p>
+                    <p>While the internet is a vast resource filled with tutorials, articles, and advice, it can often be overwhelming and impersonal. The information available might not always be reliable or specific to your situation. Online resources, while helpful, can't always provide the tailored advice or the answers we need. They lack the personal touch of a human expert who can understand your specific situation, provide customized advice, and guide you through the process. This is where the value of personalized expert advice truly shines.</p>
 
-                    <p>At Smeeple, we believe that everyone deserves access to reliable, personalized solutions for their unique problems. That's why we're committed to building a community of experts that spans a wide variety of categories, so you can find the help that's right for you. Our platform is easy to use and accessible from anywhere, so you can get the answers you need when and where it's convenient for you. Regardless of your current level of understanding in the topic, our experts are here to guide you every step of the way. Join Smeeple today and start solving your problems with the help of our amazing community of experts.</p>
+                    <p>This is why Smeeple was created. Our expert community from a wide range of categories, from DIY endeavors to sports coaching, home decoration, and more. With a few simple clicks, you can engage in a live connection with an expert capable of assisting you in resolving your issue. Say goodbye to infinite searches and generic guidance. Obtain customized solutions that cater to your needs with Smeeple.</p>
 
-                    <p>- Calvin & Josh</p>
+                    <p>At Smeeple, we believe everyone is entitled to dependable, personalized solutions for their distinct challenges. This is why we are dedicated to creating a community of experts, enabling you to find the assistance that best suits your needs. Our platform is user-friendly and accessible from any location, providing you with the answers you require at your convenience. Irrespective of your current knowledge level on the subject, our experts are ready to guide you through every stage. Become a part of Smeeple today and begin resolving your issues with the support of our exceptional community of experts.</p>
+
+                    <p>
+                        Thank you for being part of our story,
+                        <br />
+                        Calvin & Josh
+                    </p>
                 </div>
             </div>
         </div>
