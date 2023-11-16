6a2a6fbc250fb6feb63efd522f908506ae0f91f1
Ryan Altvater <ryan@ryanaltvater.com>
2023-11-16 04:09:14 -0500

    Removed unnecessary code

--- src/pages/faqs/index.vue
+++ src/pages/faqs/index.vue
@@ -9,31 +9,6 @@
             { property: 'og:url', content: '' },
         ],
     });
-
-    const accordionItems = document.querySelectorAll('.accordion-item');
-
-    accordionItems.forEach((item) => {
-        const header = item.querySelector('.accordion-header');
-        const content = item.querySelector('.accordion-content');
-
-        header.addEventListener('click', () => {
-            // close other items
-            accordionItems.forEach((otherItem) => {
-                if (otherItem !== item && otherItem.classList.contains('active')) {
-                    otherItem.classList.remove('active');
-                    otherItem.querySelector('.accordion-content').style.height = 0;
-                }
-            });
-
-            item.classList.toggle('active');
-
-            if (item.classList.contains('active')) {
-                content.style.height = `${content.scrollHeight}px`;
-            } else {
-                content.style.height = 0;
-            }
-        });
-    });
 </script>
 
 <template>
