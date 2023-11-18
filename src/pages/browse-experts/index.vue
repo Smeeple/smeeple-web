841b2f60ce812ec420799b5fec505ac07aab1d4a
Ryan Altvater <ryan@ryanaltvater.com>
2023-11-18 00:35:52 -0500

    Built out the "Browse experts" page

--- src/pages/browse-experts/index.vue
+++ src/pages/browse-experts/index.vue
@@ -1,4 +1,9 @@
 <script setup>
+    const { data: categories } = await useFetch('https://app.smeeple.com/api/v1.1/categories/mobile');
+    const toSentenceCase = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    };
+
     useHead({
         title: 'Browse experts',
         meta: [
@@ -15,7 +20,7 @@
     <section class="relative flex items-center bg-blue-dark bg-[url('/assets/img/bg-shapes-blue.png')] md-max:-mt-[92px] md-max:pt-[132px] lg:-mt-[141px] lg:pt-[221px]">
         <div class="container">
             <div class="sm:grid sm:grid-cols-12 sm:gap-3">
-                <div class="sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
+                <div class="sm:col-span-12 lg:col-span-10 lg:col-start-2">
                     <h1 class="mb-0">Browse experts</h1>
                 </div>
             </div>
@@ -27,44 +32,26 @@
     <section class="pt-0">
         <div class="container">
             <div class="sm:grid sm:grid-cols-12 sm:gap-3">
-                <div class="sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
-                    <h2>Categories</h2>
+                <div class="sm:col-span-12 lg:col-span-10 lg:col-start-2">
+                    <div class="text-center">
+                        <h2 class="mb-8 inline-block -rotate-3 -skew-x-6 rounded-lg bg-green-light px-2 text-center text-white">Categories</h2>
+                    </div>
 
-                    <h3>Business</h3>
+                    <div class="sm:grid sm:grid-cols-12 sm:gap-3">
+                        <template :key="index" v-for="(category, index) in categories">
+                            <template v-if="category.type === 'Main' && category.expertCount > 0 && category.consumerBookingActive === true">
+                                <a class="overflow-hidden rounded-lg shadow-lg shadow-black/30 sm:col-span-12 lg:col-span-4" :href="'/browse-experts/' + category.name.replace(/&/g, 'and').replace(/\s/g, '-').toLowerCase()">
+                                    <div :style="{ backgroundImage: `url('${category.image}')` }" class="card relative h-[240px] bg-cover bg-center shadow-lg shadow-black/30 duration-300">
+                                        <div class="card-body absolute bottom-0 w-full bg-white/90 px-3 py-2">
+                                            <span class="block font-poppins text-18">{{ toSentenceCase(category.name) }}</span>
 
-                    <h3>Culinary</h3>
-
-                    <h3>Digital media</h3>
-
-                    <h3>Education</h3>
-
-                    <h3>Fashion & beauty</h3>
-
-                    <h3>Health & wellness</h3>
-
-                    <h3>Home & design</h3>
-
-                    <h3>Homesteading</h3>
-
-                    <h3>Information technology</h3>
-
-                    <h3>Lawn & garden</h3>
-
-                    <h3>Music & audio production</h3>
-
-                    <h3>Outdoors</h3>
-
-                    <h3>Personal finance</h3>
-
-                    <h3>Professional services</h3>
-
-                    <h3>Sports</h3>
-
-                    <h3>Trades & services</h3>
-
-                    <h3>Travel</h3>
-
-                    <h3>Vehicle & small engine</h3>
+                                            <span class="font-poppins text-16 text-blue-light">{{ category.expertCount }} experts</span>
+                                        </div>
+                                    </div>
+                                </a>
+                            </template>
+                        </template>
+                    </div>
                 </div>
             </div>
         </div>
