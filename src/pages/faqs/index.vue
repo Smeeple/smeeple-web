92e48546fae4c6ac6b5ff7526d48eb4d00746283
Ryan Altvater <ryan@ryanaltvater.com>
2023-11-21 23:05:50 -0500

    Updated hero column widths

--- src/pages/faqs/index.vue
+++ src/pages/faqs/index.vue
@@ -15,8 +15,8 @@
     <section class="relative flex items-center bg-blue-dark bg-[url('/assets/img/bg-shapes-blue.png')] md-max:-mt-[92px] md-max:pt-[132px] lg:-mt-[141px] lg:pt-[221px]">
         <div class="container">
             <div class="sm:grid sm:grid-cols-12 sm:gap-3">
-                <div class="sm:col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
-                    <h1 class="mb-0">FAQs</h1>
+                <div class="sm:col-span-12 lg:col-span-10 lg:col-start-2">
+                    <h1 class="mb-0">Frequently asked questions</h1>
                 </div>
             </div>
         </div>
