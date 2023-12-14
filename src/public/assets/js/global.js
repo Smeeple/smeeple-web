7422a1ccbb1121157eca668ee71e883dc649dad3
Ryan Altvater <ryan@ryanaltvater.com>
2023-12-14 02:07:07 -0500

    Updated to a better approach to hide the site until everything loads

--- src/public/assets/js/global.js
+++ src/public/assets/js/global.js
@@ -1,3 +1,9 @@
+// Document ready
+document.addEventListener('DOMContentLoaded', function () {
+    // Add a class to the body, to hide it while the page is loading
+    document.body.classList.add('hidden');
+});
+
 // Unhide the body after the page is loaded
 window.addEventListener('load', (event) => {
     document.body.classList.remove('hidden');
