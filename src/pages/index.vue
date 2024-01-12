fa654baebab6ef822a9dfce4c241d87fbb88b2a9
Ryan Altvater <ryan@ryanaltvater.com>
2024-01-12 10:05:47 -0500

    Added some padding to the hero content

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,7 +16,7 @@
         <div class="container sm-max:pb-6 sm-max:pt-10 md:py-12 md-max:mt-5 lg:mt-8">
             <div class="sm:grid sm:grid-cols-12 xl:items-center xl:gap-9 2xl:gap-12">
                 <div class="sm:col-span-12 xl:col-span-6">
-                    <h1 class="mb-0 sm-max:text-40 sm-max:leading-[1.2] md:text-[60px] md:leading-none lg:text-[72px] xl:text-[60px] 2xl:text-[72px]">Connecting <span class="font-medium text-blue-light">subject</span> <span class="font-medium text-green-light">matter</span> <span class="font-medium text-orange-light">experts</span> + people</h1>
+                    <h1 class="mb-0 pb-2 sm-max:text-40 sm-max:leading-[1.2] md:text-[60px] md:leading-none lg:text-[72px] xl:text-[60px] 2xl:text-[72px]">Connecting <span class="font-medium text-blue-light">subject</span> <span class="font-medium text-green-light">matter</span> <span class="font-medium text-orange-light">experts</span> + people</h1>
 
                     <p class="mt-3">Smeeple brings you face-to-face with experts from a wide variety of fields such as interior design, fitness, HVAC, gardening, and more. Don't settle for generic online tutorials. Connect with knowledgeable experts, to get the tailored answers you&nbsp;deserve.</p>
 
