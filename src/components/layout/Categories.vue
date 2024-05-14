6a62fefd88089a2e40e65026da3527eed5844c3b
Drew Rawitz <email@drewrawitz.com>
2024-05-13 21:20:08 -0400

    copy tweaks

--- src/components/layout/Categories.vue
+++ src/components/layout/Categories.vue
@@ -8,22 +8,22 @@
 
 <template>
     <section class="pt-0 md-max:pb-7">
-        <div class="container">
+        <div class="container mx-auto max-w-[1200px]">
             <div class="sm:grid sm:grid-cols-12 sm:gap-3">
                 <div class="sm:col-span-12 xl:col-span-10 xl:col-start-2">
                     <div class="text-center">
                         <h2 class="my-6 inline-block -rotate-3 -skew-x-6 rounded-lg bg-green-light px-2 text-center text-white">Categories</h2>
                     </div>
 
-                    <div class="grid gap-3 sm:grid-cols-12">
+                    <div class="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-3">
                         <template :key="index" v-for="(category, index) in categories">
                             <template v-if="category.type === 'Main' && category.expertCount > 0 && category.consumerBookingActive === true">
-                                <a class="xs:col-span-12 overflow-hidden rounded-lg shadow-lg shadow-black/30 sm:col-span-6 lg:col-span-4" :href="'/browse-experts/' + category.name.replace(/&/g, 'and').replace(/\s/g, '-').toLowerCase()">
-                                    <div :style="{ backgroundImage: `url('${category.image}')` }" class="card relative h-[360px] bg-cover bg-center shadow-lg shadow-black/30 duration-300">
-                                        <div class="card-body absolute bottom-0 w-full bg-white/90 px-3 py-2">
-                                            <span class="block font-poppins text-18 text-black">{{ sentenceCase(category.name) }}</span>
+                                <a class="aspect-square overflow-hidden rounded-lg shadow-lg shadow-black/30" :href="'/browse-experts/' + category.name.replace(/&/g, 'and').replace(/\s/g, '-').toLowerCase()">
+                                    <div :style="{ backgroundImage: `url('${category.image}')` }" class="card relative h-full bg-cover bg-center shadow-lg shadow-black/30 duration-300">
+                                        <div class="card-body absolute bottom-0 w-full bg-white/90 px-2 py-1">
+                                            <span class="block font-poppins text-14 leading-tight text-black sm:text-16 md:text-18">{{ sentenceCase(category.name) }}</span>
 
-                                            <span class="block font-poppins text-16 text-blue-light">{{ category.expertCount }} experts</span>
+                                            <span class="block font-poppins text-12 text-blue-light sm:text-14">{{ category.expertCount }} experts</span>
                                         </div>
                                     </div>
                                 </a>
--- src/pages/about-smeeple/index.vue
+++ src/pages/about-smeeple/index.vue
@@ -29,32 +29,13 @@
                     <h2>Introduction</h2>
 
                     <div class="embed-responsive embed-responsive-16by9 relative mb-6 w-full overflow-hidden rounded-lg pt-[56.25%]">
-                        <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full" src="https://www.youtube-nocookie.com/embed/k6UZPlgQg84?mode=opaque&rel=0&autohide=1&showinfo=0&wmode=transparent" title="Signing up as an Expert is easy!" allowfullscreen></iframe>
+                        <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full" src="https://www.youtube-nocookie.com/embed/uTDq7gLUxn0?mode=opaque&rel=0&autohide=1&showinfo=0&wmode=transparent" title="Where can I get personalized help on any subject?" allowfullscreen></iframe>
                     </div>
 
                     <h2>Need solutions to unique problems?</h2>
-
-                    <!-- <figure class="sm-max:mx-auto sm-max:mb-3 sm-max:w-auto md:float-left md:mb-3 md:mr-3 md:mt-1 lg:w-[300px] xl:w-[360px]">
-                        <img class="mb-1 rounded-lg" src="/assets/img/photo-calvin-josh.jpg" alt="Photo of Calvin and Josh" />
-
-                        <figcaption>
-                            <cite>Calvin Holston (left) and Josh Mallalieu (right), graduating from Grove City College.</cite>
-                        </figcaption>
-                    </figure> -->
-
-                    <p>You're not alone. We've all found ourselves puzzled by various topics at different points in our lives. It could be something as seemingly simple as a leaky faucet that just won't stop dripping, no matter what you try. Or perhaps it's the elusive perfect golf swing that you've been striving to master, spending countless hours at the driving range with little to no improvement. It could even be the quest for the ideal skincare routine that suits your unique skin type, a routine that balances hydration and oil control, and combats the signs of aging without causing breakouts.</p>
-
-                    <p>While the internet is a vast resource filled with tutorials, articles, and advice, it can often be overwhelming and impersonal. The information available might not always be reliable or specific to your situation. Online resources, while helpful, can't always provide the tailored advice or the answers we need. They lack the personal touch of a human expert who can understand your specific situation, provide customized advice, and guide you through the process. This is where the value of personalized expert advice truly shines.</p>
-
-                    <p>This is why Smeeple was created. Our expert community from a wide variety of fields, from DIY endeavors to sports coaching, home decoration, and more. With a few simple clicks, you can engage in a live connection with an expert capable of assisting you in resolving your issue. Say goodbye to infinite searches and generic guidance. Obtain customized solutions that cater to your needs with Smeeple.</p>
-
-                    <p>At Smeeple, we believe everyone is entitled to dependable, personalized solutions for their distinct challenges. This is why we are dedicated to creating a community of experts, enabling you to find the assistance that best suits your needs. Our platform is user-friendly and accessible from any location, providing you with the answers you require at your convenience. Irrespective of your current knowledge level on the subject, our experts are ready to guide you through every stage. Become a part of Smeeple today and begin resolving your issues with the support of our exceptional community of experts.</p>
-
-                    <h4 class="mt-2">
-                        Thank you for being part of our story,
-                        <br />
-                        Calvin & Josh
-                    </h4>
+                    <p>We understand that challenges like a persistent leaky faucet, mastering a golf swing, or finding the right skincare routine can be frustrating. The internet, while vast, can be overwhelming and impersonal, often lacking reliable, tailored advice.</p>
+                    <p>That's why we created Smeeple. Our app connects you with with <span class="font-medium text-blue-light">SUBJECT</span> <span class="font-medium text-green-light">MATTER</span> <span class="font-medium text-orange-light">EXPERTS</span> (SMEs) in fields ranging from plumbing to sports to home decor. With just a few clicks, you can receive personalized advice and guidance from our experts through 1:1 video conferencing.</p>
+                    <p>Join Smeeple today and start solving your problems with the help of experts.</p>
                 </div>
             </div>
         </div>
