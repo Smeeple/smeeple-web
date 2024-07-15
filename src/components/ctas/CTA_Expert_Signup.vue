6b2d042cabbed5f310e47b62802ece42de4218b9
harshit <harshit.g@infogen-labs.com>
2024-07-15 19:31:36 +0530

    Updated dev urls

--- src/components/ctas/CTA_Expert_Signup.vue
+++ src/components/ctas/CTA_Expert_Signup.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+    import { BASE_URL } from '~/config';
+</script>
+
 <template>
-    <a class="cta-primary" href="https://app.smeeple.com/experts/create-account" target="_blank">Expert signup</a>
+    <a class="cta-primary" :href="`https://${BASE_URL}/experts/create-account`" target="_blank">Expert signup</a>
 </template>
